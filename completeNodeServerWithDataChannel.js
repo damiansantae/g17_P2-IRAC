4e3f26abe5e73e018a3ac0d799e13ca76cb4aafc
Damian <damiansantae@gmail.com>
2019-03-21 14:46:06 +0100

    trying to rute static public folder

--- completeNodeServerWithDataChannel.js
+++ completeNodeServerWithDataChannel.js
@@ -9,11 +9,10 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'client', 'index.html');
 
 
-const server = express()
-    .use((req, res) => res.sendFile(INDEX))
+const app = express();
+app.use(express.static(__dirname + '/client'));
+const server =    app.use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log('Listening on ${PORT}'));
-
-server.use(express.static(__dirname + '/client'));
 const io = socketIO(server);
 
 // Let's start managing connections...
